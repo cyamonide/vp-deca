f718941fe9605e6bb33baae69244415cb81adc6b
Andrew Wang <andrewwang298@gmail.com>
2017-08-30 13:55:01 +0000

    Add root URL

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'site/contact'
 
   get 'site/index'
+  root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
