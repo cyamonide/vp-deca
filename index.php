45caf0166c00cbf0abdc7b77866cf1ceaa82f1e0
Simon Liu <cyamonide@gmail.com>
2017-09-16 00:43:37 -0400

    Upload first working version to server

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+</head>
+  <meta charset='UTF-8'>
+  <link type='text/css' rel='stylesheet' href=''>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <title>Home</title>
+<body>
+
+  <div class="container">
+
+    <div class="jumbotron" id ="jumbo-welcome">
+      <h1 class="display-2">Welcome to Victoria Park's DECA Website!</h1>
+      <hr class="my-6">
+
+      <p class="lead">VP DECA is Victoria Park Collegiate Institiute's DECA chapter under DECA Ontario, with over 120 members in grades 9 - 12. We train and prepare our members for DECA's compeitive events, at competitions at the regional, provincial, and international levels.</p>
+
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+
+    <p></p>
+
+    <p>Our goal is to prepare our students for the rapidly changing economy by fostering high-demand skills in marketing, finance, hospitality, and business administration.</p>
+
+    <p>DECA prepares emerging leaders and entrepreneurs in marketing, finance, hospitality and management in high schools and colleges around the globe.</p>
+
+  </div>
+
+</body>
+
+</html>
