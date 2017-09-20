6f725d27e1d359e7f4c894c916a08706fb5d3372
Andrew Wang <andrewwang298@gmail.com>
2017-09-20 03:31:41 +0000

    Embed auto popup typeform and create TODO

---
+++ TODO.md
@@ -0,0 +1,15 @@
+********************************************************************************
+        ********    ********        TODO         *******     *******
+********************************************************************************
+
+    Higher priority
+
+- Redesign main page jumbotron (slideshow?)
+- Add custom logo to header
+- Clean up registration page
+
+    Lower priority
+    
+- Write up about page
+- Write up contact page
+- Put up link to dropbox on resources page
--- registration.php
+++ registration.php
@@ -4,7 +4,7 @@
 
 <head>
 
-  <meta charset='UTF-8'>
+  <meta charset='UTF-8' name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -17,23 +17,53 @@
 
   <link type='text/css' rel='stylesheet' href='assets/stylesheets/custom.css'>
 
-  <title>About Us | VP DECA</title>
-
+  <title>Registration | VP DECA</title>
 </head>
 
 <body>
 
   <?php require($DOCUMENT_ROOT . "assets/layouts/_header.php"); ?>
 
-<div class="container-background">
-
+  <!-- Typeform auto popup embed -->
+  <a class="typeform-share button" href="https://vpdeca.typeform.com/to/roGKkg" 
+  data-mode="popup" data-auto-open=true data-hide-headers=true 
+  data-hide-footer=true target="_blank"> </a> <script> 
+  (function() { var qs,js,q,s,d=document, gi=d.getElementById,
+  ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share",
+  b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); 
+  js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0];
+  q.parentNode.insertBefore(js,q) } })() </script>
+  
+  
+  <!-- button for accidental close -->
   <div class="container">
-
-    <p> Sorry, but this page doesn't exist. Good luck next time. </p>
-
+    
+    <div class="jumbotron" id ="jumbo-welcome">
+      
+      <h1 class="page-title">Registration</h1>
+      <hr>
+      
+      <p>
+      Welcome to the registration page! Click the button to begin the typeform.
+      Please take this seriously.
+      </p>
+   
+      <a class = "typeform-share button" 
+      href="https://vpdeca.typeform.com/to/roGKkg"
+      data-mode="popup"
+      data-hide-headers=true data-hide-footer=true target="_blank"
+      style="display:inline-block;text-decoration:none;background-color:#375a7f;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:16px;line-height:40px;text-align:center;margin:0;height:40px;padding:0px 26px;border-radius:2px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;"
+      >
+      Register 
+      </a> 
+   
+      <script> (function() { var qs,js,q,s,d=document, 
+      gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName,
+      id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id))
+      { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; 
+      q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+   
   </div>
-
-</div>
 
   <?php require($DOCUMENT_ROOT . "assets/layouts/_footer.php"); ?>
 
