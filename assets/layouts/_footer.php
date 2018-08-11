7758ee3b27caaed2f213ee92f4befbfc558922dc
Simon Liu <cyamonide@gmail.com>
2018-08-11 09:28:09 -0400

    Update footer executives link

--- assets/layouts/_footer.php
+++ assets/layouts/_footer.php
@@ -1,12 +1,12 @@
 <footer class="footer">
   <small>
     The <a href="https://github.com/cyamonide/vp-deca">Victoria Park DECA website</a>
-    by <a href="https://github.com/Cyamonide">Simon Liu</a>
+    by <a href="https://github.com/cyamonide">Simon Liu</a>
   </small>
   <nav>
     <ul>
       <li><a href="/index.php">Home</a></li>
-      <li><a href="/exec.php">Executives</a></li>
+      <li><a href="/executives.php">Executives</a></li>
       <li><a href="/events.php">Upcoming Events</a></li>
       <li><a href="/members.php">Members</a></li>
       <li><a href="/contact.php">Contact</a></li>
